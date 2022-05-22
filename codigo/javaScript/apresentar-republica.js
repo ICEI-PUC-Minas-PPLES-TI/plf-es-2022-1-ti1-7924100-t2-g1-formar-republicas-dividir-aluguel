647ac67361fc40bb602de6d968a1bcb06991b50c
Carlos Emanuel <carlos.oliveira.140549@sga.pucminas.br>
2022-05-22 10:53:55 -0300

    Adicionar IIFE no JS

--- codigo/javaScript/apresentar-republica.js
+++ codigo/javaScript/apresentar-republica.js
@@ -1,5 +1,6 @@
-//let btnBuscar = document.querySelector("#btn-buscar");
-btnBuscar.addEventListener("click",(event)=>{
+(()=>{
+    let btnBuscar = document.querySelector("#btn-buscar");
+    btnBuscar.addEventListener("click",(event)=>{
 
     event.preventDefault();
 
@@ -9,27 +10,43 @@ btnBuscar.addEventListener("click",(event)=>{
 
     btnBuscar.disabled = true;
 
-});
-
-function MontarRepublica(republica){
-    let tabela = document.querySelector("#tabela-republica");
-    tabela.innerHTML = " ";
-   
-    republica.republica.forEach(element => {
-        let novaLinha = document.createElement("tr");
-
-        novaLinha.appendChild(CriarCampo(element.nome));
-        novaLinha.appendChild(CriarCampo(element.numero));
-        novaLinha.appendChild(CriarCampo(element.local));
-        novaLinha.appendChild(CriarCampo(element.valor));
-        novaLinha.appendChild(CriarCampo(element.status));
-
-        tabela.appendChild(novaLinha);
     });
-}
 
-function CriarCampo(conteudo){
-    let novoCampo = document.createElement("td");
-    novoCampo.textContent = conteudo; 
-    return novoCampo;
-}
+    function CarregarRepublicas(){
+        let dadosRepublicas = localStorage.getItem('db');
+        let objRepublica = {};
+
+        if(dadosRepublicas != null){
+            objRepublica = JSON.parse(dadosRepublicas);
+        }
+
+        else{
+            objRepublica = { republica:[]}
+        };
+
+        return objRepublica;
+    }
+
+    function MontarRepublica(republica){
+        let tabela = document.querySelector("#tabela-republica");
+        tabela.innerHTML = " ";
+    
+        republica.republica.forEach(element => {
+            let novaLinha = document.createElement("tr");
+
+            novaLinha.appendChild(CriarCampo(element.nome));
+            novaLinha.appendChild(CriarCampo(element.numero));
+            novaLinha.appendChild(CriarCampo(element.local));
+            novaLinha.appendChild(CriarCampo(element.valor));
+            novaLinha.appendChild(CriarCampo(element.status));
+
+            tabela.appendChild(novaLinha);
+        });
+    }
+
+    function CriarCampo(conteudo){
+        let novoCampo = document.createElement("td");
+        novoCampo.textContent = conteudo; 
+        return novoCampo;
+    }
+})()
--- codigo/javaScript/criar-republica.js
+++ codigo/javaScript/criar-republica.js
@@ -1,7 +1,8 @@
-let btnCriar = document.querySelector("#btn-cadastrar");
-let btnBuscar = document.querySelector("#btn-buscar");
+( ()=>{
+    let btnCriar = document.querySelector("#btn-cadastrar");
+    let btnBuscar = document.querySelector("#btn-buscar");
 
-btnCriar.addEventListener("click", (event)=>{
+    btnCriar.addEventListener("click", (event)=>{
     //Não atualizar a página
     event.preventDefault();
   
@@ -22,55 +23,57 @@ btnCriar.addEventListener("click", (event)=>{
    }
 });
 
-function ExtrairInformacoes(formulario){
-    let republica = {
-        nome: formulario.nome.value,
-        numero: formulario.numero.value,
-        local: formulario.localizacao.value,
-        valor: formulario.valor.value,
-        status: formulario.status.value
-    };
+    function ExtrairInformacoes(formulario){
+        let republica = {
+            nome: formulario.nome.value,
+            numero: formulario.numero.value,
+            local: formulario.localizacao.value,
+            valor: formulario.valor.value,
+            status: formulario.status.value
+        };
 
-    return republica
-}
-
-function CarregarRepublicas(){
-    let dadosRepublicas = localStorage.getItem('db');
-    let objRepublica = {};
-
-    if(dadosRepublicas != null){
-        objRepublica = JSON.parse(dadosRepublicas);
+        return republica
     }
 
-    else{
-        objRepublica = { republica:[]}
-    };
+    function CarregarRepublicas(){
+        let dadosRepublicas = localStorage.getItem('db');
+        let objRepublica = {};
 
-    return objRepublica;
-}
+        if(dadosRepublicas != null){
+            objRepublica = JSON.parse(dadosRepublicas);
+        }
 
-function CriarRepublica(republica){
-    let objRepublica = CarregarRepublicas();
-    objRepublica.republica.push(republica);
-    
-    let novaRepublica = objRepublica;
-    SalvarRepublica(novaRepublica);
-}
+        else{
+            objRepublica = { republica:[]}
+        };
 
-function SalvarRepublica(republica){
-    localStorage.setItem('db', JSON.stringify(republica));
-}
-
-function ValidarFormulario(){
-    let nome = document.forms["form"]["nome"].value;
-    let numero = document.forms["form"]["numero"].value;
-    let localizacao = document.forms["form"]["localizacao"].value;
-    let valor = document.forms["form"]["valor"].value;
-
-    if (nome == "" || numero == "" || localizacao == "" || valor == ""){
-        alert("Preencha todo o formúlario");
-        return false;
+        return objRepublica;
     }
 
-    return true;
-}
+    function CriarRepublica(republica){
+        let objRepublica = CarregarRepublicas();
+        objRepublica.republica.push(republica);
+        
+        let novaRepublica = objRepublica;
+        SalvarRepublica(novaRepublica);
+    }
+
+    function SalvarRepublica(republica){
+        localStorage.setItem('db', JSON.stringify(republica));
+        alert("República criada com sucesso.")
+    }
+
+    function ValidarFormulario(){
+        let nome = document.forms["form"]["nome"].value;
+        let numero = document.forms["form"]["numero"].value;
+        let localizacao = document.forms["form"]["localizacao"].value;
+        let valor = document.forms["form"]["valor"].value;
+
+        if (nome == "" || numero == "" || localizacao == "" || valor == ""){
+            alert("Preencha todo o formúlario");
+            return false;
+        }
+
+        return true;
+    }
+})()
